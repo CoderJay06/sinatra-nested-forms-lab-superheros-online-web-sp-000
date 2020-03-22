957730bffc1a047a9fcd3fee3dec016a1381c21c
Jason J Cruz <jaycru06@yahoo.com>
2020-03-22 04:55:04 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_03_14_064051) do
+
+  create_table "heros", force: :cascade do |t|
+    t.integer "team_id"
+    t.string "name"
+    t.string "power"
+    t.string "bio"
+    t.index ["team_id"], name: "index_heros_on_team_id"
+  end
+
+  create_table "teams", force: :cascade do |t|
+    t.string "name"
+    t.string "motto"
+  end
 
 end
